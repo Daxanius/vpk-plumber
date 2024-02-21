0ddcd88059f93b8e7eeae6b2406e58e975d73d04
Barnaby <22575741+barnabwhy@users.noreply.github.com>
2024-02-21 20:45:02 +0000

    add display to PakFormat

--- src/common/detect.rs
+++ src/common/detect.rs
@@ -1,15 +1,30 @@
+use std::fmt;
+
 #[cfg(feature = "revpk")]
 use crate::pak::revpk::format::VPKHeaderRespawn;
 use crate::pak::{v1::format::VPKHeaderV1, v2::format::VPKHeaderV2};
 
 use super::file::VPKFile;
 
-#[derive(Debug, PartialEq)]
+#[derive(PartialEq)]
 pub enum PakFormat {
     Unknown,
     VPKVersion1,
     VPKVersion2,
     VPKRespawn,
+}
+
+impl fmt::Display for PakFormat {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let str = match self {
+            PakFormat::Unknown => "Unknown",
+            PakFormat::VPKVersion1 => "VPK Version 1",
+            PakFormat::VPKVersion2 => "VPK Version 2",
+            PakFormat::VPKRespawn => "VPK Respawn",
+        };
+        
+        write!(f, "{}", str)
+    }
 }
 
 pub fn detect_pak_format(file: &mut VPKFile) -> PakFormat {
