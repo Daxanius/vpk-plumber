cb3840fa4647cc57d364bbf648db71b9ec5a3d8b
Daxanius <balder.huybreghs@gmail.com>
2025-04-07 01:49:49 +0200

    Readd memory map tests

---
+++ tests/revpk/mem_map.rs
@@ -0,0 +1,38 @@
+use std::{fs::File, io::Read};
+
+use vpk_plumber::pak::{PakReader, revpk::VPKRespawn};
+
+use crate::common::{self, Result};
+
+use filebuffer::FileBuffer;
+use std::collections::HashMap;
+
+#[test]
+fn extract_vpk_single_file() -> Result<()> {
+    let mut file = File::open(common::PAK_REVPK_SINGLE_FILE)?;
+    let vpk = VPKRespawn::try_from(&mut file)?;
+
+    let mut archive_mmaps = HashMap::new();
+    archive_mmaps.insert(0, FileBuffer::open(common::PAK_REVPK_ARCHIVE).unwrap());
+
+    let out_path = tempfile::NamedTempFile::new()?;
+
+    vpk.extract_file_mem_map(
+        common::DIR_REVPK,
+        &archive_mmaps,
+        common::SINGLE_FILE_ARCHIVE,
+        common::SINGLE_FILE_NAME,
+        out_path.path().to_str().unwrap(),
+    )?;
+
+    let mut result = String::new();
+    File::open(&out_path)?.read_to_string(&mut result)?;
+
+    assert_eq!(
+        result,
+        common::SINGLE_FILE_CONTENT,
+        "File contents should match {out_path:#?}",
+    );
+
+    Ok(())
+}
--- tests/v2/mem_map.rs
+++ tests/v2/mem_map.rs
@@ -1,1 +1,39 @@
+use std::{fs::File, io::Read};
 
+use vpk_plumber::pak::{PakReader, v2::VPKVersion2};
+
+use crate::common::{self, Result};
+
+use filebuffer::FileBuffer;
+use std::collections::HashMap;
+
+#[ignore = "not yet implemented"]
+#[test]
+fn extract_vpk_single_file() -> Result<()> {
+    let mut file = File::open(common::PAK_V2_SINGLE_FILE)?;
+    let vpk = VPKVersion2::try_from(&mut file)?;
+
+    let mut archive_mmaps = HashMap::new();
+    archive_mmaps.insert(0, FileBuffer::open(common::PAK_V2_ARCHIVE).unwrap());
+
+    let out_path = tempfile::NamedTempFile::new()?;
+
+    vpk.extract_file_mem_map(
+        common::DIR_V2,
+        &archive_mmaps,
+        common::SINGLE_FILE_ARCHIVE,
+        common::SINGLE_FILE_NAME,
+        out_path.path().to_str().unwrap(),
+    )?;
+
+    let mut result = String::new();
+    File::open(&out_path)?.read_to_string(&mut result)?;
+
+    assert_eq!(
+        result,
+        common::SINGLE_FILE_CONTENT,
+        "File contents should match {out_path:#?}",
+    );
+
+    Ok(())
+}
